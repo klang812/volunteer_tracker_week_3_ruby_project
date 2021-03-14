c275b4b85b1aef1f3e985d21cd2fd2a2a0a72d64
Kerry Lang <klang812@gmail.com>
2021-03-14 11:04:23 -0700

    pass all integration tests

--- spec/volunteer_integration_spec.rb
+++ spec/volunteer_integration_spec.rb
@@ -49,22 +49,17 @@ end
 
 # # The user should be able to click on a project detail page and see a list of all volunteers working on that project. The user should be able to click on a volunteer to see the volunteer's detail page.
 
-# describe 'the volunteer detail page path', {:type => :feature} do
-#   it 'shows a volunteer detail page' do
-#     test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-#     test_project.save
-#     project_id = test_project.id.to_i
-#     test_volunteer = Volunteer.new({:name => 'Jasmine', :project_id => project_id, :id => nil})
-#     test_volunteer.save
-#     visit "/projects/#{project_id}"
-#     click_link('Jasmine')
-    
-#     fill_in('name', :with => 'Jane')
-
-#     click_button('Update Volunteer')
-#     save_and_open_page
-
-#     expect(page).to have_content('Jane')
-    
-#   end
-# end
+describe 'the volunteer detail page path', {:type => :feature} do
+  it 'shows a volunteer detail page' do
+    test_project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+    test_project.save
+    project_id = test_project.id.to_i
+    test_volunteer = Volunteer.new({:name => 'Jasmine', :project_id => project_id, :id => nil})
+    test_volunteer.save
+    visit "/projects/#{project_id}"
+    click_link('Jasmine')
+    fill_in('name', :with => 'Jane')
+    click_button('Update Volunteer')
+    expect(page).to have_content('Jane')
+  end
+end
